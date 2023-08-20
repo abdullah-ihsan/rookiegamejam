2be34f9cb8c5e32d270dc815169137a27066c6e6
abdullah-ihsan <65601738+abdullah-ihsan@users.noreply.github.com>
2023-08-20 14:40:23 +0500

    some updates
    -> added particle effect to gun and replaced red and blue line with a black line for aiming
    -> updated scoreboard
    -> some other minor tweaks
    -> added some sound scripts (work in progress)

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -23,6 +23,7 @@ public class GunScript : MonoBehaviour
     private EnemyMovement enemy;
     private Canvas enemyBar;
     private LineRenderer _laserLine;
+    private ParticleSystem gunParticles; //
 
     private Rigidbody rb;
     private Animator _animator;
@@ -33,12 +34,15 @@ public class GunScript : MonoBehaviour
     private void Awake(){
 //        _button.onClick.AddListener(Shoot);
         _laserLine = GetComponent<LineRenderer>();
+        gunParticles = GetComponentInChildren<ParticleSystem>();
+        gunParticles.Stop();
         _player = _laserLine.GetComponentInParent<PlayerMovement>();
     }
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
+        
         //StartCoroutine(StartShooting());
     }
 
@@ -68,31 +72,33 @@ public class GunScript : MonoBehaviour
         
         if ((Physics.Raycast(_laserpoint.position, rb.transform.forward, out hit, range)) && (hit.collider.gameObject.CompareTag("Enemy") || hit.collider.gameObject.CompareTag("UnderAttack")))
         {
+            gunParticles.Play();
             enemy = hit.transform.gameObject.GetComponent<EnemyMovement>();
-            Debug.DrawRay(rb.transform.position, rb.transform.forward*hit.distance ,Color.red);
+            //Debug.DrawRay(rb.transform.position, rb.transform.forward*hit.distance ,Color.red);
             
             //Debug.Log(hit.transform.name + " shot");
 
             enemyBar = hit.transform.gameObject.GetComponentInChildren<Canvas>();
             enemyBar.enabled = true;
-            _laserLine.startColor = Color.red;
-            _laserLine.endColor = Color.red;
-            _laserLine.startWidth = 0.5f;
-            _laserLine.endWidth = 0.5f;
+            //_laserLine.startColor = Color.red;
+            //_laserLine.endColor = Color.red;
+            //_laserLine.startWidth = 0.5f;
+            //_laserLine.endWidth = 0.5f;
 
             _player.setMovementSpeed(shootMovementSpeed);
-
+            
             enemy.takeDamage(damage);
             //_animator.SetTrigger("isShot");
            
         }
         else
         {
+            gunParticles.Stop();
             _player.setMovementSpeed(normalMovementSpeed);
-            _laserLine.startWidth = 0.2f;
-            _laserLine.endWidth = 0.2f;
-            _laserLine.startColor = Color.blue;
-            _laserLine.endColor = Color.blue;
+            _laserLine.startWidth = 0.1f;
+            _laserLine.endWidth = 0.1f;
+            _laserLine.startColor = Color.black;
+            _laserLine.endColor = Color.black;
             if(enemyBar != null && enemyBar.enabled)
                 enemyBar.enabled = false;
         }
